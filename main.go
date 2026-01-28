30f583bbc5ef2f53836d255074702823664138f1
pithandev <pithanbala7@gmail.com>
2026-01-28 18:22:23 -0300

    we can get match ids now

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 )
+
+type AccountResponse struct {
+	PUUID string `json:"puuid"`
+}
 
 func main() {
 
@@ -28,6 +34,22 @@ func statsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var account AccountResponse
+
+	err = json.Unmarshal(data, &account)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	puuid := account.PUUID
+
+	matchIds, err := fetchMatchIDs(puuid)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	json.NewEncoder(w).Encode(matchIds)
 	w.Write(data)
 
 }
@@ -46,4 +68,34 @@ func fetchFromRiot(summoner string) ([]byte, error) {
 
 	defer resp.Body.Close()
 	return io.ReadAll(resp.Body)
+}
+
+func fetchMatchIDs(puuid string) ([]string, error) {
+	apiKey := os.Getenv("RIOT_API_KEY")
+
+	escapedPUUID := url.PathEscape(puuid)
+
+	url := "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" +
+		escapedPUUID + "/ids?start=0&count=20"
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("X-Riot-Token", apiKey)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("riot api error: status %d", resp.StatusCode)
+	}
+
+	var matchIDs []string
+
+	err = json.NewDecoder(resp.Body).Decode(&matchIDs)
+
+	return matchIDs, nil
+
 }
